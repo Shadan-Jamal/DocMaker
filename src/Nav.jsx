aab67921fbcf55992c35f1f1579b7c728ae8654b
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-05-09 00:27:35 +0530

    Add nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,1 +1,19 @@
 import React from "react";
+import Font,{Text} from "react-font";
+
+export default function Nav(){
+    const buttons=[
+        {
+            id:"Add",
+            
+        }
+    ]
+    return (
+        <Font family="Poetsen One">
+            <header className="text-black font-bold h-16 text-start px-9 py-3 text-2xl tracking-wider bg-white">DocMaker</header>
+            <div>
+                button
+            </div>
+        </Font>
+    )
+}
