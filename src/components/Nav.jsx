c9ca03ec33b9968679d938a6866fd504a9918570
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-05-09 21:06:54 +0530

    add popup card

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,11 +10,12 @@ export default function Nav(){
     const [showPopup,setShowPopup]=useState(false);
     return (
         <Font family="Poetsen One">
-            <header className="text-white font-bold h-16 text-start px-4 py-3 text-3xl tracking-wider bg-gray-700">DocMaker</header>
-            <div className="w-screen h-14 flex flex-row justify-start items-center gap-6 bg-white border-t-2 border-t-black">
+            <header className="text-white font-bold h-16 text-start px-4 py-3 text-3xl tracking-wider bg-gray-700">DocMaker.</header>
+            <div className="w-screen h-14 flex flex-row justify-start items-center gap-6 bg-white/90">
                 <motion.button 
                 className="rounded-full p-3 transition-colors hover:bg-gray-600/50 ms-4"
-                onClick={() => setShowPopup(!showPopup)}><FaPlus size={"20px"} />
+                onClick={() => setShowPopup(!showPopup)}>
+                    <FaPlus size={"20px"} />
                 </motion.button>
 
                 <motion.button className="rounded-full p-3 transition-colors hover:bg-gray-600/50"><FaMagnifyingGlass size={"20px"} />
@@ -23,7 +24,8 @@ export default function Nav(){
                 <motion.button className="rounded-full p-3 transition-colors hover:bg-gray-600/50"><FaXmark size={"22px"} />
                 </motion.button>
             </div>
-            {showPopup && <Popupcard/>}
+            {showPopup && 
+            <Popupcard />}
         </Font>
     )
 }
--- src/components/Popupcard.jsx
+++ src/components/Popupcard.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import {motion,AnimatePresence} from "framer-motion";
 import "../styles.css"
 
 const Popupcard =  () =>{
-    return (
-        <motion.div
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:0.5}}
-        className="bg-white text-black w-96 absolute top-1/2 right-1/2 p-5 rounded-lg"
-      >
-        <label htmlFor="title" className="text-black">Enter Title:</label>
-        <input type="text" id="title" className="border-b-2 border-b-black text-black w-full h-11 focus:noborders"/>
-      </motion.div>  
-    )
+    const [checkKey,setCheckKey]=useState('');
+
+      window.addEventListener('keydown', (e)=>{
+        if(e.code === "Enter")
+          setCheckKey("Enter");
+      });
+
+      return <Card />
 };
+
+function Card(){
+  return <motion.div
+  initial={{opacity:0,scale:0}}
+  animate={{opacity:1,scale:1}}
+  transition={{duration:0.1}}
+  exit={{opacity:0,scale:0}}
+  className="bg-white text-black w-96 absolute top-1/2 right-1/2 p-5 rounded-lg"
+>
+  <label htmlFor="title" className="text-black">Enter Title:</label>
+  <input 
+  type="text" 
+  id="title"
+  className="border-b-2 border-b-black text-black w-full h-11 noborders"/>
+
+</motion.div>  
+}
+
 export default Popupcard;
